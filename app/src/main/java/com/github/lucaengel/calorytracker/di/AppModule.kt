5fef3d658578051a94a5025b99fae340fcdf7ba0
LucaS <luca.engel@epfl.ch>
2024-03-10 21:01:17 +0100

    add preferences and start dagger hilt

---
+++ app/src/main/java/com/github/lucaengel/calorytracker/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.github.lucaengel.calorytracker.di
+
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import com.github.lucaengel.core.data.preferences.DefaultPreferences
+import com.github.lucaengel.core.domain.preferences.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(
+        app: Application
+    ): SharedPreferences {
+        return app.getSharedPreferences("shared_pref", Context.MODE_PRIVATE)
+    }
+
+    @Provides
+    @Singleton
+    fun providePreferences(
+        sharedPreferences: SharedPreferences
+    ): Preferences {
+        return DefaultPreferences(sharedPreferences)
+    }
+}
